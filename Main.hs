708f7c0315538c3fd203b6466a4e57654e6e8d12
Jacob Jonsson <jassob@live.se>
2016-06-15 22:37:39 +0200

    Implemented some kind of committing thingie.

--- Main.hs
+++ Main.hs
@@ -31,8 +31,29 @@ findRevisions f = readPmExportFile f >>= (return . revsToList)
 printHelp :: IO ()
 printHelp = undefined
 
-commitRevs :: HM.HashMap String Revision -> String -> IO ()
-commitRevs map dest = undefined
+commitRevs :: [Revision] -> FilePath -> IO ()
+commitRevs (r:revs) dest = do
+  commitRev r dest
+  commitRevs revs dest
 
-commitRev :: Revision -> String -> String -> IO ()
-commitRev rev author msg = undefined
+commitRev :: Revision -> FilePath -> IO ()
+commitRev rev path = let encodeContent = E.encodeUtf8 . content
+                         (<+>) = T.append
+  in do
+  C.writeFile path (encodeContent rev)
+  callCommand ("git add " ++ path)
+  callCommand $ T.unpack ("git commit -m " <+> msg
+                        <+> " --author=" <+> commitAuthor
+                        <+> " --date=" <+> commitDate <+> " +0200")
+    where msg = comment rev
+          commitAuthor = author rev
+          commitDate = (T.pack . show . diff) rev
+
+-- | Translates the map to a list
+revsToList :: HM.HashMap String Revision -> [Revision]
+revsToList = reverse . (flip revsToList' 0)
+
+revsToList' :: (Show a, Num a) => HM.HashMap String Revision -> a -> [Revision]
+revsToList' map i = case HM.lookup (show i) map of
+                  (Just rev) -> rev : revsToList' map (i + 1)
+                  Nothing  -> []
